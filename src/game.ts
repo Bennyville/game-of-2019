74da0e44f28fc013001b9c568faea67988212ded
Benny Peckruhn <benny@bennyville.de>
2019-11-11 11:39:27 +0100

    increase gravity

--- src/game.ts
+++ src/game.ts
@@ -11,7 +11,7 @@ const config: Phaser.Types.Core.GameConfig = {
         default: "arcade",
         arcade: {
             gravity: {
-                y: 100
+                y: 1250
             }
         }
     }
