8354ba4b5d61e1b27ac6e9958d3881213eefc8b6
Benny Peckruhn <benny@bennyville.de>
2019-11-11 10:37:43 +0100

    handle input

--- src/objects/player.ts
+++ src/objects/player.ts
@@ -3,11 +3,14 @@ import "phaser"
 export class Player extends Phaser.GameObjects.Graphics {
     private position: Phaser.Math.Vector2;
     private velocity: Phaser.Math.Vector2;
+    private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
 
     constructor(scene: Phaser.Scene) {
         super(scene);
 
         this.position = new Phaser.Math.Vector2({x: 0, y: 0});
+
+        this.cursors = scene.input.keyboard.createCursorKeys();
 
         this.fillStyle(0xffffff, 1);
         this.fillRect(0, 0, 20, 20);
@@ -18,6 +21,22 @@ export class Player extends Phaser.GameObjects.Graphics {
         if (this.body instanceof Phaser.Physics.Arcade.Body) {
             this.body.setSize(20, 20);
             this.body.collideWorldBounds = true;
+        }
+    }
+
+    handleInput() {
+        if(this.cursors.down.isDown) {
+            this.dodge();
+        }
+
+        if(this.cursors.up.isDown) {
+            this.jump();
+        }
+
+        if(this.cursors.left.isDown) {
+            this.moveLeft();
+        } else if(this.cursors.right.isDown) {
+            this.moveRight();
         }
     }
 }
