ecd6508a2303219a24619df49c26b344ca53b4d1
Benny Peckruhn <benny@bennyville.de>
2019-11-16 00:57:06 +0100

    rename spritesheets

--- src/scenes/bootScene.ts
+++ src/scenes/bootScene.ts
@@ -8,9 +8,9 @@ export class BootScene extends Phaser.Scene {
     }
 
     preload(): void {
-        this.load.spritesheet("player", "../src/assets/player-walking.png", {frameHeight: 17, frameWidth: 16});
-        this.load.spritesheet("enemy", "../src/assets/enemy_set.png", {frameHeight: 17, frameWidth: 16});
-        this.load.spritesheet("weapons", "../src/assets/weapons_set.png", {frameHeight: 16, frameWidth: 24});
+        this.load.spritesheet("player", "../src/assets/player.png", {frameHeight: 17, frameWidth: 16});
+        this.load.spritesheet("enemy", "../src/assets/enemies.png", {frameHeight: 17, frameWidth: 16});
+        this.load.spritesheet("weapons", "../src/assets/weapons.png", {frameHeight: 16, frameWidth: 24});
         this.load.image("background", "../src/assets/background.png");
     }
 
--- src/scenes/mainMenuScene.ts
+++ src/scenes/mainMenuScene.ts
@@ -16,13 +16,6 @@ export class MainMenuScene extends Phaser.Scene {
         );
     }
 
-    preload(): void {
-        this.load.spritesheet("player", "../src/assets/player-walking.png", {frameHeight: 17, frameWidth: 16});
-        this.load.spritesheet("enemy", "../src/assets/enemy_set.png", {frameHeight: 17, frameWidth: 16});
-        this.load.spritesheet("weapons", "../src/assets/weapons_set.png", {frameHeight: 16, frameWidth: 24});
-        this.load.image("background", "../src/assets/background.png");
-    }
-
     create(): void {
         this.texts.push(
             this.add.text(
