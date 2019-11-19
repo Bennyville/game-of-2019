4eaf186a6be5c8e0682109641e5662c3c2103e05
Benny Peckruhn <benny@bennyville.de>
2019-11-19 20:26:45 +0100

    merge

--- src/scenes/mainMenuScene.ts
+++ src/scenes/mainMenuScene.ts
@@ -17,6 +17,8 @@ export class MainMenuScene extends Phaser.Scene {
     }
 
     create(): void {
+        this.add.tileSprite(400, 300, 800, 600, "background");
+
         this.texts.push(
             this.add.text(
                 this.sys.canvas.width / 2 - 100,
