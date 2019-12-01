1013900808e2ecd6319fd1cd7c2f81e7cf61bb68
Benny Peckruhn <benny@bennyville.de>
2019-12-01 20:21:56 +0100

    fix paths for itch.io

--- src/scenes/bootScene.ts
+++ src/scenes/bootScene.ts
@@ -8,13 +8,13 @@ export class BootScene extends Phaser.Scene {
     }
 
     preload(): void {
-        this.load.spritesheet("player", "../src/assets/player.png", {frameHeight: 34, frameWidth: 32});
-        this.load.spritesheet("enemy", "../src/assets/enemies.png", {frameHeight: 34, frameWidth: 32});
-        this.load.spritesheet("weapons", "../src/assets/weapons.png", {frameHeight: 32, frameWidth: 48});
-        this.load.image("background", "../src/assets/background.png");
-        this.load.image("tiles", "../src/assets/environment.png");
-        this.load.audio("step", "../src/assets/110099__ronaldvanwonderen__heavy-footstep.wav");
-        this.load.audio("shot", "../src/assets/77340__tcpp__machinegun-ish.wav");
+        this.load.spritesheet("player", "src/assets/player.png", {frameHeight: 34, frameWidth: 32});
+        this.load.spritesheet("enemy", "src/assets/enemies.png", {frameHeight: 34, frameWidth: 32});
+        this.load.spritesheet("weapons", "src/assets/weapons.png", {frameHeight: 32, frameWidth: 48});
+        this.load.image("background", "src/assets/background.png");
+        this.load.image("tiles", "src/assets/environment.png");
+        this.load.audio("step", "src/assets/110099__ronaldvanwonderen__heavy-footstep.wav");
+        this.load.audio("shot", "src/assets/77340__tcpp__machinegun-ish.wav");
     }
 
     update(): void {
